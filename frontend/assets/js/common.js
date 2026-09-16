36e5b44ff4d392716647c5818daf0d7309d896aa
dhanapriyakolli-gif <dhanapriyakolli@gmail.com>
2026-09-16 18:39:27 +0530

    common.js

--- frontend/assets/js/common.js
+++ frontend/assets/js/common.js
@@ -4,7 +4,7 @@
    COMMON.JS
 ========================================== */
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://kanya-sarees-backend.onrender.com";
 /* ==========================================
    LOGIN CHECK
 ========================================== */

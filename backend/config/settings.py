6507777b5175d2ed551b5dd17a223731637101c3
Dhanapriya <dhanapriyakolli@gmail.com>
2026-09-17 15:12:03 +0530

    Clean up email configuration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -163,20 +163,6 @@ CORS_ALLOWED_ORIGINS = [
     "https://kanya-sarees-frontend.onrender.com",
 ]
 
-# Email
-# https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
